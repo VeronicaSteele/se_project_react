6e6826cf708dce3e002a6a77296c2b7b24a3f66e
VeronicaSteele <vmariesteele@gmail.com>
2024-01-07 20:43:16 -0600

    WeatherCard added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,14 @@
 // import logo from "";
 import "./App.css";
 import Header from "../Header/Header";
+import WeatherCard from "../WeatherCard/WeatherCard";
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <section id="weather">
-          <div>75F</div>
-          <div>
-            <img />
-          </div>
-        </section>
+      <WeatherCard day={false} type="cloudy" />
+      <main className="main">
         <section id="card-section">Card Section</section>
       </main>
     </div>
